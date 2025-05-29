9616ab9c44e67cae589ee953558fde45031ebbed
nickhung <nick@microsys.com.tw>
2025-05-29 18:06:27 +0800

    feat:
    1.init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "@/views/Home.vue";
+import Home from "@/views/home/index.vue";
 
 const routes = [{ path: "/", name: "Home", component: Home }];
 
